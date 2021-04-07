1161df26dd6a1aec93211b7e0413c14a3e7d9e01
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-07 12:15:31 +0300

    Connect authentication templates

--- authentication/views.py
+++ authentication/views.py
@@ -5,4 +5,5 @@ from django.views import View
 # Create your views here.
 
 class RegistrationView(View):
-
+    def get(self, request):
+        return render(request, 'authentication/register.html')
