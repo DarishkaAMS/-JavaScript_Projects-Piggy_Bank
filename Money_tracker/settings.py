6f4a2f3573216c4b4e4452d7ec3c0a0b9e81e875
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-07 13:13:44 +0300

    Set up .env

--- Money_tracker/settings.py
+++ Money_tracker/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,12 +78,13 @@ WSGI_APPLICATION = 'Money_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
-        'USER': 'postgres',
-        'PASSWORD': 'dummy_password',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
     }
 }
@@ -127,3 +130,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Money_tracker/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Money_tracker.wsgi
